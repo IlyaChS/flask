eab02cbeeb6a788d0aa85418305223dabfdb9b67
Ilya_ChS <chistyak06@gmail.com>
2024-02-05 19:39:02 +0400

    third task

--- index.py
+++ index.py
@@ -18,6 +18,22 @@ def promote():
            '<br>И начнем с Марса!</br>' \
            '<br>Присоединяйся!</br>'
 
+@app.route('/image_mars')
+def image():
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars.png')}" 
+                        alt="здесь должна была быть картинка, но не нашлась">
+                        <br>Вот она какая, красная планета.</br>
+                      </body>
+                    </html>'''
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
