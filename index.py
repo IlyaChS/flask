a33e00f0ba5059b9724ad96d8f614953897c9cce
Ilya_ChS <chistyak06@gmail.com>
2024-02-09 19:54:48 +0400

    для доработки 2.2

--- index.py
+++ index.py
@@ -10,5 +10,10 @@ def start_page():
     return render_template('index.html', title='Заготовка')
 
 
+@app.route('/training/<prof>')
+def trainings(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
