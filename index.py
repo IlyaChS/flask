4d210f7e30ebdc252baa02b118e8d788615feabb
Ilya_ChS <chistyak06@gmail.com>
2024-02-12 19:24:15 +0400

    task 2.3

--- index.py
+++ index.py
@@ -15,5 +15,13 @@ def trainings(prof):
     return render_template('training.html', prof=prof)
 
 
+@app.route('/list_prof/<t>')
+def list_prof(t):
+    profs = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+             'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог', 'инженер жизнеобеспечения',
+             'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман', 'пилот дронов']
+    return render_template('professions.html', t=t, profs=profs)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
